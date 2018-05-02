8a51f1732276264ebe3e94aa16f6859e17a91217
anthoche55 <38587318+anthoche55@users.noreply.github.com>
2018-05-02 10:07:20 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,13 +1,15 @@
 const Discord = require('discord.js');
 
 var bot = new Discord.Client();
+var prefix = ("*");
 
 bot.on("ready",function() {
-    bot.user.setGame("anonyme,§help");
-    console.log("Le bot est bien ete connecte");
+    bot.user.setGame("anonyme,*help");
+    console.log("Le bot est bien été connecté");
 });
 
 bot.login("NDQwMDk0NTQwNDE0NTgyNzg1.DcfJhw.3s50KKQiJ1tFPefw6Cml49jTh7k");
+
 
 bot.on('message',message => {
     if (message.content === prefix + "salut ca va ?"){
@@ -45,10 +47,54 @@ bot.on('message',message => {
     }
 
     if (message.content === prefix + "help"){
-        message.channel.send('voici les commandes du bot :\n -§help pour afficher les commandes');
-        console.log("commande help demmandée !");
+        message.channel.send('voici les commandes du bot :\n -*help pour afficher les commandes');
+        message.channel.send('\n -*ban pour bannir un personne');
+        message.channel.send('\n -*kick pour kicker un personne du serveur');
+        console.log("liste des commandes demander !");
     }
 });
+
+
+bot.on('message', message => {
+    let command = message.content.split(" ")[0];
+    const args = message.content.slice(prefix.length).split(/ +/);
+    command = args.shift().toLowerCase();
+
+    if (command === "kick") {
+        let modRole = message.guild.roles.find("name", "👑👮Leader👮👑");
+        if(!message.member.roles.has(modRole.id)) {
+            return message.reply("Tu n'as pas la permission de faire cette commande.").catch(console.error);
+        }
+        if(message.mentions.users.size === 0) {
+            return message.reply("Merci de mentionner l'utilisateur à expluser.").catch(console.error);
+        }
+        let kickMember = message.guild.member(message.mentions.users.first());
+        if(!kickMember) {
+            return message.reply("Cet utilisateur est introuvable ou impossible à expluser.")
+        }
+        if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
+            return message.reply("Je n'ai pas la permission KICK_MEMBER pour faire ceci.").catch(console.error);
+        }
+        kickMember.kick().then(member => {
+            message.reply(`${member.user.username} a été expulsé avec succès.`).catch(console.error);
+            message.guild.channels.find("name", "commande-bot").send(`**${member.user.username} a été expulsé du discord par **${message.author.username}**`)
+        }).catch(console.error) 
+    
+    }
+
+    if (command === "ban") {
+        let modRole = message.guild.roles.find("name", "👑👮Leader👮👑");
+        if(!message.member.roles.has(modRole.id)) {
+            return message.reply("Tu n'as pas la permission de faire cette commande.").catch(console.error);
+        }
+        const member = message.mentions.members.first();
+        if (!member) return message.reply("Merci de mentionner l'utilisateur à bannir.");
+        member.ban().then(member => {
+            message.reply(`${member.user.username} a été banni avec succès.`).catch(console.error);
+            message.guild.channels.find("name", "commande-bot").send(`**${member.user.username}** a été banni du discord par **${message.author.username}**`)
+        }).catch(console.error)
+
+    }})
 
 bot.on("guildMemberAdd", member => {
     member.guild.channel.find("name", "Bienvenue").send(`Bienvenue ${member}`)
